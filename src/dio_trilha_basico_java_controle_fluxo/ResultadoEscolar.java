fb6bedf734393ee591ca065b67097f2d51e36130
maurolqz@gmail.com <maurolqz@gmail.com>
2025-07-07 17:37:21 -0300

    feat:add if encadeado.07/07/25-17:35.

--- src/dio_trilha_basico_java_controle_fluxo/ResultadoEscolar.java
+++ src/dio_trilha_basico_java_controle_fluxo/ResultadoEscolar.java
@@ -3,12 +3,21 @@ package dio_trilha_basico_java_controle_fluxo;
 public class ResultadoEscolar {
 
 	public static void main(String[] args) {
+		/*
 		int nota  = 6;
 		if(nota >= 7) 
 			System.out.println("Aprovado");
 		else
-			System.out.println("Reprovado");
+			System.out.println("Reprovado");*/
+		int nota  = 8;
+		if(nota >= 7) 
+			System.out.println("Aprovado!");
+		else if(nota>=5 && nota<7)
+			System.out.println("Prova Recuperação!");
+		else
+			System.out.println("Reprovado!");
 	}
 }
+
 
 
