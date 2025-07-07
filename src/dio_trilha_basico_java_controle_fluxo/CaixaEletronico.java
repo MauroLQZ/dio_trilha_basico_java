b21b37fc4b8d8fcdec50f35e34ac2513bccfb5d8
maurolqz@gmail.com <maurolqz@gmail.com>
2025-07-07 09:27:18 -0300

    feat: add classe ResultadoEscolar e modificando classe CaixaEletronica.

--- src/dio_trilha_basico_java_controle_fluxo/CaixaEletronico.java
+++ src/dio_trilha_basico_java_controle_fluxo/CaixaEletronico.java
@@ -5,9 +5,16 @@ public class CaixaEletronico {
 	public static void main(String[] args) {
 		double saldo = 25.0;
 		   double valorSolicitado = 17.0;
+		   /*Condicional Simples
 		   if(valorSolicitado <= saldo) {
 			   saldo -= valorSolicitado;
 		   }
-		   System.out.println(saldo);
+		   System.out.println(saldo);*/
+		   /*Condicional Composta */
+		   if(valorSolicitado <= saldo) {
+			   saldo -= valorSolicitado;
+			   System.out.println(saldo);
+		   }else
+			   System.out.println("Saldo Insuficiente: "+saldo);
 	}
 }
---
+++ src/dio_trilha_basico_java_controle_fluxo/ResultadoEscolar.java
@@ -0,0 +1,10 @@
+package dio_trilha_basico_java_controle_fluxo;
+
+public class ResultadoEscolar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
