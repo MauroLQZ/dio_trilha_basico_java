3b84d05b945980c059f2b95a4fb61d7530795f4b
maurolqz@gmail.com <maurolqz@gmail.com>
2025-07-08 10:50:41 -0300

    feat:atualizando classe ResultadoEscolar.08/07/25-11:00.

--- src/dio_trilha_basico_java_controle_fluxo/ResultadoEscolar.java
+++ src/dio_trilha_basico_java_controle_fluxo/ResultadoEscolar.java
@@ -9,15 +9,20 @@ public class ResultadoEscolar {
 			System.out.println("Aprovado");
 		else
 			System.out.println("Reprovado");*/
-		int nota  = 8;
+		/*int nota  = 8;
 		if(nota >= 7) 
 			System.out.println("Aprovado!");
 		else if(nota>=5 && nota<7)
 			System.out.println("Prova Recuperação!");
 		else
-			System.out.println("Reprovado!");
-	}
-}
+			System.out.println("Reprovado!");*/
+		int nota  = 6;
+		String resultado = nota>=7?"Aprovado":nota>=5&&nota<7?"Recuperação":"Reprovado";
+		System.out.println(resultado);
+	}		
+}	
+		
+	
 
 
 
