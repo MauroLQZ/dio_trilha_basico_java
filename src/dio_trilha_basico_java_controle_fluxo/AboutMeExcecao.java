ed5c20d024204dce90b8656d0acc968b577daf4a
maurolqz@gmail.com <maurolqz@gmail.com>
2025-07-10 17:20:34 -0300

    feat:add classes AboutMeExcecao, CepInvalidoException,FormatadorCepExemplo trabalhando com Exceptions.10/07/25 - 17:00

---
+++ src/dio_trilha_basico_java_controle_fluxo/AboutMeExcecao.java
@@ -0,0 +1,30 @@
+package dio_trilha_basico_java_controle_fluxo;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMeExcecao {
+
+	public static void main(String[] args) {
+		try {
+		   Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
+		   System.out.println("Digite seu Nome:");
+		   String nome = scanner.next();
+		   System.out.println("Digite seu sobre SobreNome:");
+		   String sobreNome = scanner.next();
+		   System.out.println("Digite sua idade:");
+		   int idade = scanner.nextInt();
+		   System.out.println("Digite sua altura:");
+		   double altura = scanner.nextDouble();
+		   //Imprimindo os dados obtidos pelo usuario
+		   System.out.println("Ola, me chamo "+nome.toUpperCase()+" "+sobreNome.toUpperCase());
+		   System.out.println("Tenho "+idade+" anos");
+		   System.out.println("Minha altura é "+altura+" cm ");
+           scanner.close();
+		} catch(InputMismatchException e){
+     		  System.out.println("Os campos idade e algura precisam ser numéricos!");
+     	}
+	}
+
+}
---
+++ src/dio_trilha_basico_java_controle_fluxo/CepInvalidoException.java
@@ -0,0 +1,5 @@
+package dio_trilha_basico_java_controle_fluxo;
+
+public class CepInvalidoException extends Exception{
+
+}
