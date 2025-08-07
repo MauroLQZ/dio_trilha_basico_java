9e4d0a898a4260f425cefb3a9430f6f41debcccc
maurolqz@gmail.com <maurolqz@gmail.com>
2025-08-07 10:45:51 -0300

    feat:add classes abstratas.-07/08/25-10:45

---
+++ src/dio_trilha_basico_java/poo/abstrata/MSNMessenger.java
@@ -0,0 +1,24 @@
+package dio_trilha_basico_java.poo.abstrata;
+
+public class MSNMessenger extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		// primeiro confirmar se esta conectado a internet
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem pelo Messenger!");
+		// depois de enviada,salva o histórico da msg
+		salvarHistoricoMensagem();
+	}
+
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Messenger!");
+	}
+
+	private void validarConectadoInternet() {
+		System.out.println("Validando se esta Conectado a Internet!");
+	}
+
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando o Histórico de  mensagens!");
+	}
+
+}
---
+++ src/dio_trilha_basico_java/poo/abstrata/ServicoMensagemInstantanea.java
@@ -0,0 +1,7 @@
+package dio_trilha_basico_java.poo.abstrata;
+
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();
+
+}
