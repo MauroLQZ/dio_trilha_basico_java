aa96a9f85971c4e024ce81e9d4b6c3a9a4a854ac
maurolqz@gmail.com <maurolqz@gmail.com>
2025-08-07 09:44:12 -0300

    feat:add pacote poo.-07/08/25-09:40 Estudo de POO.

---
+++ src/dio_trilha_basico_java/poo/Veiculo.java
@@ -0,0 +1,13 @@
+package dio_trilha_basico_java.poo;
+
+public abstract class Veiculo {
+	 private String chassi;
+	 public String getChassi() {
+		return this.chassi;	
+	 }
+	 public void setChassi(String chassi) {
+		this.chassi = chassi;	
+	 }
+	 public abstract void ligar();
+ }
+ 
