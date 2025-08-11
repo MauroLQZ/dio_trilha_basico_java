88e03c28e5e78481fd4690ffa7a1d8a45e8a51c6
maurolqz@gmail.com <maurolqz@gmail.com>
2025-08-11 19:55:23 -0300

    feat:update classes modificadas.11/08/25-19:55

---
+++ src/dio_trilha_basico_java/desafio/ContaTerminal.java
@@ -0,0 +1,102 @@
+package dio_trilha_basico_java.desafio;
+
+import java.util.Locale;
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
+
+import dio_trilha_basico_java.ContaBanco;
+
+/** O programa deverá simular uma operação de saque onde.
+ * Terá uma variável denominada saldo do tipo double contendo o valor inicial 
+ * igual a 25;
+ * Terá uma variável denominada de valor solicitado do tipo double contendo 
+ * o valor inicial igual a 18;
+ * Criar uma expressão relacional para que caso o saldo seja maior que o valor
+ * solicitado, o saldo recebe o novo valor como saldo é igual saldo menos o 
+ * valor solicitado. Caso o saldo seja menor que o valor solicitado, devemos 
+ * exibir a mensagem "Saldo insuficiente";
+ * Imprima o valor do saldo;
+ * Em seguida, realize a mesma execução do programa agora com os valores 
+ * saldo igual a 15 e valor solicitado igual a 22 e exiba o resultado; */
+public class ContaTerminal {
+	public static void main(String[] args) {
+		Scanner resposta = new Scanner(System.in);
+		resposta.useLocale(Locale.US);
+		ContaBanco conta = new ContaBanco();
+		System.out.println("Qual nome do cliente?");
+		String name = resposta.next();
+		conta.setCliente(name);
+		System.out.println("Digite a agencia?");
+		String agencia = resposta.next();
+		conta.setAgencia(agencia);
+		System.out.println("Qual o numero da conta?");
+		int numero = resposta.nextInt();
+		conta.setNumero(numero);
+		System.out.printf("Valor do Saldo?");
+		double saldo = resposta.nextDouble();
+		conta.setSaldo(saldo);
+		System.out.printf("Valor do Saque?");
+		double valorSaque = resposta.nextDouble();
+		conta.setValorSolicitado(valorSaque);
+		
+		System.out.println("Cliente: "+conta.getCliente());
+		System.out.println("Agencia: "+conta.getAgencia());
+		System.out.println("Numero C/C: "+conta.getNumero());
+		//System.out.printf("\nPeso Ideal = %.2f kgs\n", psIdeal);
+		System.out.printf("Saldo atual: %.2f",conta.getSaldo()); 
+		System.out.printf("\nValor Solicitado para saque: %.2f",conta.getValorSolicitado());
+		System.out.printf("\nValor do saque: %.2f",conta.getValorSolicitado());
+		//System.out.printf("Saque solicitado: %.2f",conta.saque());
+		System.out.printf("\nSaque solicitado: %.2f",conta.getValorSolicitado()," - ",conta.saque());
+		System.out.printf("\nSaldo Final: %.2f",conta.getSaldo());
+		System.out.println("\n==========================================================");
+		System.out.println("Balanço:");
+		System.out.println(conta.relatorio(conta));
+		resposta.close();
+		/*conta.setSaldo(15);
+		conta.setValorSolicitado(22);
+		System.out.println("Saldo atual: "+conta.getSaldo());
+		System.out.println("Valor Solicitado para saque: "+conta.getValorSolicitado());
+	    System.out.println("Valor do saque: "+conta.getValorSolicitado());
+	    System.out.println("Saque solicitado: "+conta.saque());
+	    System.out.println("Saldo Final: "+conta.getSaldo());*/	
+//------------------------------------------------------------------------
+		/*String name = JOptionPane.showInputDialog("Qual nome do cliente?");
+		conta.setCliente(name);
+		String agencia = JOptionPane.showInputDialog("Qual a Agencia?");
+		conta.setAgencia(agencia);
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Qual numero da conta?"));
+		conta.setNumero(numero);
+		double saldo = Double.parseDouble(JOptionPane.showInputDialog("Digite o saldo?"));
+		conta.setSaldo(saldo);
+		double valorSaque = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor para saque?"));
+		conta.setValorSolicitado(valorSaque);
+		//System.out.println("Cliente: "+conta.getCliente());
+		JOptionPane.showMessageDialog(null,"Cliente: "+conta.getCliente());
+		//System.out.println("Agencia: "+conta.getAgencia());
+		JOptionPane.showMessageDialog(null,"Agencia: "+conta.getAgencia());
+		//System.out.println("Numero C/C: "+conta.getNumero());
+		JOptionPane.showMessageDialog(null,"Numero: "+conta.getNumero());
+		//System.out.println("Saldo atual: "+conta.getSaldo());
+		JOptionPane.showMessageDialog(null,"Saldo: "+conta.getSaldo());
+		//System.out.println("Valor Solicitado para saque: "+conta.getValorSolicitado());
+		JOptionPane.showMessageDialog(null,"Valor solicitdo para saque: "+conta.getValorSolicitado());
+		//System.out.println("Valor do saque: "+conta.getValorSolicitado());
+		JOptionPane.showMessageDialog(null,"Valor do saque: "+conta.getValorSolicitado());
+	    //System.out.println("Saque solicitado: "+conta.saque());
+		JOptionPane.showMessageDialog(null,"Saque solicitado: "+conta.saque());
+	    //System.out.println("Saldo Final: "+conta.getSaldo());
+		JOptionPane.showMessageDialog(null,"Saldo final: "+conta.getSaldo());
+	    //System.out.println("Balanço:");
+		JOptionPane.showMessageDialog(null,"Balanço!");
+		//System.out.println(conta.toString());
+		JOptionPane.showMessageDialog(null,conta.relatorio(conta)); */
+		/*conta.setSaldo(15);
+		conta.setValorSolicitado(22);
+		System.out.println("Saldo atual: "+conta.getSaldo());
+		System.out.println("Valor Solicitado para saque: "+conta.getValorSolicitado());
+	    System.out.println("Valor do saque: "+conta.getValorSolicitado());
+	    System.out.println("Saque solicitado: "+conta.saque());
+	    System.out.println("Saldo Final: "+conta.getSaldo());*/
+	}
+}
---
+++ src/dio_trilha_basico_java/desafio/MainBanco.java
@@ -0,0 +1,109 @@
+package dio_trilha_basico_java.desafio;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte
+ * forma: new Scanner(System.in): cria um leitor de Entradas, com métodos úteis
+ * com prefixo "next"; System.out.println:.imprime um texto de Saída (Output) e
+ * pulando uma linha.
+ * --------------------------------------------------------------------------------------
+ * Implemente um programa em Java que receba o saldo inicial de uma conta
+ * bancária e três transações (positivas para depósitos e negativas para
+ * saques). O programa deve calcular e exibir o saldo final da conta. Entrada: O
+ * programa deve receber: Um número decimal representando o saldo inicial. rês
+ * números decimais representando os valores das transações. Saída: O saldo
+ * final da conta bancária após as três transações. Exemplos: A tabela abaixo
+ * apresenta exemplos c/alguns dados de entrada e suas respectivas saídas
+ * esperadas. Certifique-se de testar seu programa com esses exemplos e com
+ * outros casos possíveis. 
+ * Entrada:             | Saída 
+ * 100.00               | 
+ * 50.00 -20.00 -30.00  | 100.00 
+ * -----------------------------
+ * 200.50              
+ * -50.50 -70.00 30.00  | 110.00
+ * -----------------------------
+ * 0.00                 | 
+ * 100.00 -50.00 -25.00 | 25.00
+ */
+public class MainBanco {
+	/*
+	 * Implemente um programa em Java que receba o saldo inicial de uma conta
+	 * bancária e três transações (positivas para depósitos e negativas para
+	 * saques). O programa deve calcular e exibir o saldo final da conta. Entrada: O
+	 * programa deve receber: Um número decimal representando o saldo inicial. três
+	 * números decimais representando os valores das transações. Saída: O saldo
+	 * final da conta bancária após as três transações.
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+			double saldoInicial = 0;
+			double saldoFinal = 0;
+			double deposito = 0;
+			double saque = 0;
+			boolean teste = true;
+			do {
+				System.out.println("Digite o valor do saldo inicial da conta: ");
+				saldoInicial = scanner.nextDouble();
+				if (saldoInicial < 0) {
+					System.out.println("O valor deve ser positivo.");
+					scanner.nextLine();
+					continue;
+				}
+				System.out.println("Digite o valor do deposito: ");
+				deposito = scanner.nextDouble();
+				if (deposito <= 0) {
+					System.out.println("O valor deve ser positivo.");
+					scanner.nextLine();
+					continue;
+				}
+				saldoInicial += deposito;
+				//System.out.println("Deposito realizado no valor: " + deposito);
+				System.out.printf("Deposito realizado no valor: %.2f\n",deposito);
+				System.out.printf("Saldo da conta: %.2f\n",saldoInicial);
+	
+				System.out.println("Digite o valor do primeiro saque: ");
+				saque = scanner.nextDouble();
+				if (saque <= 0) {
+					System.out.println("O valor deve ser positivo.");
+					scanner.nextLine();
+					continue;
+				}
+				if (saque > saldoInicial) {
+					System.out.println("Saldo insuficiente! Nao foi possivel saque!");
+				} else {
+					saldoInicial -= saque;
+					System.out.printf("Primeiro saque realizado no valor: %.2f\n",saque);
+					System.out.printf("Saldo da conta: %.2f\n",saldoInicial);
+				}
+	
+				System.out.println("Digite o valor do segundo saque: ");
+				saque = scanner.nextDouble();
+				if (saque <= 0) {
+					System.out.println("O valor deve ser positivo.");
+					scanner.nextLine();
+					continue;
+				}
+				if (saque > saldoInicial) {
+					System.out.println("Saldo insuficiente! Nao foi possivel saque!");
+				} else {
+					saldoInicial -= saque;
+					System.out.printf("Segundo saque realizado no valor: %.2f\n",saque);
+					System.out.printf("Saldo da conta: %.2f\n",saldoInicial);
+				}
+				teste = false;
+			}while(teste);
+	            saldoFinal = saldoInicial;
+	            System.out.printf("Saldo final da conta: %.2f\n", saldoFinal);
+				scanner.close();
+		}//Fim try
+		catch (InputMismatchException e) {
+			System.out.println(e.getMessage() + ": Digite somente numeros!");
+		}//Fim catch
+	}//Fim do metodo main
+}//-Fim classe Main
